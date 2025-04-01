49423b3865fa246f466ccae041895ae9d7b6bfc9
225108954 <s225108954@deakin.edu.au>
2025-04-01 16:21:16 +1100

    Changes

--- Tast4.2PCode/server.js
+++ Tast4.2PCode/server.js
@@ -6,16 +6,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
-// MongoDB Connection
 mongoose.connect('mongodb://127.0.0.1:27017/sit725')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Product Schema
 const productSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -23,12 +19,10 @@ const productSchema = new mongoose.Schema({
 });
 const Product = mongoose.model('Product', productSchema);
 
-// Root route handler
 app.get('/', (req, res) => {
     res.send('Welcome to the SIT 725 Week 3 project!');
 });
 
-// GET API to Fetch Card Data
 app.get('/products', async (req, res) => {
     try {
         const products = await Product.find();
@@ -38,7 +32,6 @@ app.get('/products', async (req, res) => {
     }
 });
 
-// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
